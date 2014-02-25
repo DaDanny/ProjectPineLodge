39f11f2dc0605f7b03c7d90889ed21aa59c61500
DaDanny <DanFrancken@gmail.com>
2014-02-25 08:00:28 -0500

    Finished socket IO Chat. Now need to change up layout of chat room

--- lvSite/app.js
+++ lvSite/app.js
@@ -8,6 +8,8 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var chat = require('./routes/chat');
+var socketio = require('socket.io');
 
 var app = express();
 
@@ -35,7 +37,87 @@ app.get('/users', user.list);
 app.get('/boot', routes.bootstrap);
 app.get('/test',routes.test);
 app.get('/old',routes.old);
+app.get('/chat',chat.main);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = app.listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+var io = socketio.listen(server);
+var clients = {};
+
+
+var socketsOfClients = {};
+io.sockets.on('connection', function(socket){
+	socket.on('set username', function(userName){
+		if(clients[userName] == undefined){
+			clients[userName] = socket.id;
+			socketsOfClients[socket.id] = userName;
+			userNameAvailable(socket.id,userName);
+			userJoined(userName);
+		}
+		else{
+			userNameAlreadyInUse(socket.id,userName);
+		}
+	});
+
+	socket.on('message', function(msg){
+		var srcUser;
+		if(msg.inferSrcUser){
+			srcUser = socketsOfClients[socket.id];
+		}
+		else{
+			srcUser = msg.source;
+		}
+		//User wants to send message to everyone in chat
+		if(msg.target == "All"){
+			io.sockets.emit('message',
+			{
+				"source": srcUser,
+				"message": msg.message,
+				"target": msg.target
+			});
+		}
+		//User is sending message to specific user
+		else{
+			io.sockets.sockets[clients[msg.target]].emit('message',
+			{
+				"source": srcUser,
+				"message": msg.message,
+				"target": msg.target
+			});
+
+		}
+	})
+
+	socket.on('disconnect', function(){
+		var uName = socketsOfClients[socket.id];
+		delete socketsOfClients[socket.id];
+		delete clients[uName];
+
+		userLeft(uName);
+	})
+})
+
+function userJoined(uName){
+	Object.keys(socketsOfClients).forEach(function(sId){
+		io.sockets.sockets[sId].emit('userJoined',{"userName": uName});
+	})
+}
+
+function userLeft(uName){
+	io.sockets.emit('userLeft',{"userName":uName});
+}
+
+function userNameAvailable(sId, uName){
+	setTimeout(function(){
+		console.log('Sending welcome msg to ' + uName + ' at '+sId);
+		io.sockets.sockets[sId].emit('welcome', {"userName":uName,"currentUsers": JSON.stringify(Object.keys(clients))});
+	}, 500);
+}
+
+function userNameAlreadyInUse(sId, uName){
+	setTimeout(function(){
+		io.sockets.sockets[sId].emit('error',{"userNameInUse" :true});
+	}, 500);
+};
